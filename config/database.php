0375c115b91caa278df2c1c1c866e3d540aac791
zihad? <114378270+AlbarruSahZihad@users.noreply.github.com>
2026-04-19 17:50:00 +0700

    database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,23 @@
 <?php
-$host = 'localhost';
-$dbname = 'kos';
-$username = 'root';
-$password = '';
+$mysql_url = getenv('MYSQL_URL');
+
+if ($mysql_url) {
+    $url = parse_url($mysql_url);
+    $host = $url['host'];
+    $port = $url['port'] ?? 3306;
+    $dbname = ltrim($url['path'], '/');
+    $username = $url['user'];
+    $password = $url['pass'];
+} else {
+    $host = 'localhost';
+    $dbname = 'kos';
+    $username = 'root';
+    $password = '';
+    $port = 3306;
+}
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
